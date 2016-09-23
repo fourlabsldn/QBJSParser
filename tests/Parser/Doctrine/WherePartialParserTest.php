339b324434b686f578d68bde2317f70fb80fe2dc
Luis Galeas <galeaspablo@hotmail.com>
2016-09-23 16:53:52 +0100

    where partial parser test

---
+++ tests/Parser/Doctrine/WherePartialParserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FL\QBJSParser\Tests\Parser\Doctrine;
+
+/**
+ * These are covered in @see DoctrineParserTest
+ */
+
+class WherePartialParserTest extends \PHPUnit_Framework_TestCase
+{
+}
